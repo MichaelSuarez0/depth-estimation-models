0da2bd468895979f6f6b3f74f2b927ccbd47b2b9
MichaelSuarez0 <a20180264@pucp.edu.pe>
2026-09-09 13:09:31 -0500

    refactor: añade set_normalization método para scripts anteriores

--- scripts/depth_any_img.py
+++ scripts/depth_any_img.py
@@ -1,19 +1,23 @@
 from pathlib import Path
 
-from depth_estimation_models import ModelBuilder
+from depth_estimation_models import DepthModelBuilder
 
 ROOT = Path(__file__).parent
 OUTPUT_PATH = ROOT / "output"
+OUTPUT_PATH.mkdir(exist_ok=True)
+
 IMAGE_PATH = ROOT / "img1.jpeg"
 OUT_GRAY = OUTPUT_PATH / "depth_gray_img1b.png"
 OUT_COLOR = OUTPUT_PATH / "depth_color_img1b.png"
 
-OUTPUT_PATH.mkdir(exist_ok=True)
-
 
 def main():
-    mb = ModelBuilder(model_id="depth-anything/Depth-Anything-V2-Base-hf")
-    mb.build_model().infer_depth_from_img(IMAGE_PATH, OUT_GRAY, OUT_COLOR)
+    mb = DepthModelBuilder(model_id="depth-anything/Depth-Anything-V2-Base-hf")
+    (
+        mb.build_model()
+        .set_normalization("relative")
+        .infer_depth_from_img(IMAGE_PATH, OUT_GRAY, OUT_COLOR)
+    )
 
 
 if __name__ == "__main__":
--- scripts/depth_any_rt.py
+++ scripts/depth_any_rt.py
@@ -1,9 +1,14 @@
-from depth_estimation_models import ModelBuilder
+from depth_estimation_models import DepthModelBuilder
 
 
 def main():
-    mb = ModelBuilder(model_id="depth-anything/Depth-Anything-V2-Small-hf")
-    mb.build_model().set_video_capture().run_video_capture(cmap_name="inferno")
+    mb = DepthModelBuilder(model_id="depth-anything/Depth-Anything-V2-Small-hf")
+    (
+        mb.build_model(half=True)
+        .set_normalization("relative")
+        .set_video_capture()
+        .run_video_capture(cmap_name="inferno")
+    )
 
 
 if __name__ == "__main__":
